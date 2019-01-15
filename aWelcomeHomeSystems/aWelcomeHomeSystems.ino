25f49473b4cd187ad9b24097186e1a14c2e36377
Martin Rossau <s164058@student.dtu.dk>
2019-01-15 15:33:13 +0100

    RFID code

--- aWelcomeHomeSystems/aWelcomeHomeSystems.ino
+++ aWelcomeHomeSystems/aWelcomeHomeSystems.ino
@@ -7,7 +7,43 @@
 
 
 // RFID setup:
+/*
+ *
+ * Uses MIFARE RFID card using RFID-RC522 reader
+ * Uses MFRC522 - Library
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+ *
+ *
+ *
+ * NodeMCU
+ * // SDA/SS -> D2(GPIO 4)
+ * // SCK -> D5(GPIO 14)
+ * // MOSI -> D7(GPIO 13)
+ * // MISO/SCL -> D6(GPIO 12)
+ * // IRQ -> NOT CONNECTED
+ * // GND -> GND
+ * // RST -> D1(GPIO 5)
+ * // VCC -> 3.3V
+*/
 
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN         5           // Configurable, see typical pin layout above
+#define SS_PIN          53          // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
+
+byte readCard[4];   // Stores scanned ID read from RFID Module
 
 
 // WiFi setup:
@@ -54,8 +90,8 @@ void setup() {
 
 
   // RFID setup:
-
-
+  SPI.begin(); // Start SPI bus
+  mfrc522.PCD_Init(); // Start RFID reader
 
   // WiFi setup:
   WiFi.mode(WIFI_STA);
--- aWelcomeHomeSystems/cRFID.ino
+++ aWelcomeHomeSystems/cRFID.ino
@@ -1,1 +1,30 @@
 // RFID
+if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+  Serial.println(F("**Card Detected:**"));
+
+ //-------------------------------------------
+
+  for ( uint8_t i = 0; i < 4; i++) {  //
+   readCard[i] = mfrc522.uid.uidByte[i];
+   Serial.print(readCard[i], HEX);
+ }
+ Serial.println("");
+
+ //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));      //uncomment this to see all blocks in hex
+
+ //-------------------------------------------
+
+
+
+ Serial.println(F("\n**End Reading**\n"));
+ delay(1000); //change value if you want to read cards faster
+
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
