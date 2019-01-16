926d90f7ea1c01e1262d7124b46518ea5f1c1346
andreasgamborg <s163939@student.dtu.dk>
2019-01-16 14:57:26 +0100

    All states added

--- aWelcomeHomeSystems/aWelcomeHomeSystems.ino
+++ aWelcomeHomeSystems/aWelcomeHomeSystems.ino
@@ -4,9 +4,16 @@
 
 // Std. setup_____________________________________________________________________________________:
 // State definitions
-#define BT 1
-#define WRITE 2
-#define WAIT 3
+#define BT 101
+#define NFC 102
+#define NFC_MASTER 103
+#define NFC_NEW 104
+#define WELCOME 105
+#define WAIT 106
+#define WRONG 107
+#define NEW_USER 108
+
+
 #define RST_PIN         5           // Configurable, see typical pin layout above
 #define SS_PIN          53          // Configurable, see typical pin layout above
 
@@ -95,10 +102,10 @@ SoftwareSerial BTserial(D6, D7); // (SERIAL_RX, SERIAL_TX) CONNECT TO (BT_TX, BT
 void setup() {
   // Std. setup_____________________________________________________________________________________:
   Serial.begin(115200); // Starts a serial connection
-  currentState = BT;
-  nextState = BT;
+  currentState = WAIT;
+  nextState = WAIT;
   startTime = millis();   // Save starting time
-  first = true;
+  first = true;           // Variable for init of states
 
   // Bluetooth setup_____________________________________________________________________________________:
   BT_setup();
@@ -139,8 +146,6 @@ char BTmac[] = "00xxx0000000"; //4E4424073BB7 //6CB4F55C9646 //5F7F9129578C
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //UID = rfidfunc(mfrc522, 1);
-  //Serial.println(UID);
   //Next state?
   if (nextState != currentState) {
     startTime = millis();
@@ -148,33 +153,99 @@ void loop() {
     first = true;
   }
   timeElapsed = millis() - startTime;
-
+  //UID = rfidfunc(mfrc522, 1);
+  //Serial.println(UID);
   // State Logic
   switch (currentState) {
+    //-----------------------------------------------------------------------------------------------
     case BT:
       Serial.println("Scanning BT");
       BT_last(BTmac);
       nextState = WAIT;
       break;
+    //-----------------------------------------------------------------------------------------------
+    case NFC:
+      if (first) {
+        // Init of state
+        // Runs only one time
+        first = false;
+      }
+      // State
+      if (timeElapsed > 1000) { //Need timing? [ms]
+        nextState = WAIT;
+      }
+      break;
+    //-----------------------------------------------------------------------------------------------
+    case NFC_MASTER:
+      if (first) {
+        // Init of state
+        // Runs only one time
+        first = false;
+      }
+      // State
+      if (timeElapsed > 1000) { //Need timing? [ms]
+        nextState = WAIT;
+      }
+      break;
+    //-----------------------------------------------------------------------------------------------
+    case NFC_NEW:
+      if (first) {
+        // Init of state
+        // Runs only one time
+        first = false;
+      }
+      // State
+      if (timeElapsed > 1000) { //Need timing? [ms]
+        nextState = WAIT;
+      }
+      break;
+    //-----------------------------------------------------------------------------------------------
+    case WELCOME:
+      if (first) {
+        // Init of state
+        // Runs only one time
+        first = false;
+      }
+      // State
+      if (timeElapsed > 1000) { //Need timing? [ms]
+        nextState = WAIT;
+      }
+      break;
+    //-----------------------------------------------------------------------------------------------
     case WAIT:
       if (first) {
         Serial.println("Waiting . . .");
         first = false;
       }
       if (timeElapsed > 2000) {
-        nextState = WRITE;
+        nextState = WAIT;
       }
       break;
-    case WRITE:
+    //-----------------------------------------------------------------------------------------------
+    case WRONG:
       if (first) {
-        Serial.print("Found: ");
-        Serial.println(BTmac);
+        // Init of state
+        // Runs only one time
         first = false;
       }
-      if (timeElapsed > 6000) {
-        nextState = BT;
+      // State
+      if (timeElapsed > 1000) { //Need timing? [ms]
+        nextState = WAIT;
       }
       break;
+    //-----------------------------------------------------------------------------------------------
+    case NEW_USER:
+      if (first) {
+        // Init of state
+        // Runs only one time
+        first = false;
+      }
+      // State
+      if (timeElapsed > 1000) { //Need timing? [ms]
+        nextState = WAIT;
+      }
+      break;
+    //-----------------------------------------------------------------------------------------------
     default:
       Serial.println("Unknown state!");
       nextState = WAIT;
