12287848d4ed3fe731cb524895560cf731d720af
Martin Rossau <s164058@student.dtu.dk>
2019-01-16 14:52:51 +0100

    RFID added complete

--- aWelcomeHomeSystems/cRFID.ino
+++ aWelcomeHomeSystems/cRFID.ino
@@ -1,38 +1,54 @@
-byte * rfidfunc(MFRC522 mfrc522, int debug) { // RFID
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    return;
-  }
 
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
-    return;
-  }
-  if (debug == 1) {
-    Serial.println(F("**Card Detected:**"));
-  }
+/*
 
-  //-------------------------------------------
+   Uses MIFARE RFID card using RFID-RC522 reader
+   Uses MFRC522 - Library
+   -----------------------------------------------------------------------------------------
+               MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+               Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+   Signal      Pin          Pin           Pin       Pin        Pin              Pin
+   -----------------------------------------------------------------------------------------
+   RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+   SPI SS      SDA(SS)      10            53        D10        10               10
+   SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+   SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+   SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
 
-  for ( uint8_t i = 0; i < 4; i++) {  //
-    readCard[i] = mfrc522.uid.uidByte[i];
-    if (debug == 1) {
-      Serial.print(readCard[i], HEX);
+
+
+   NodeMCU
+   // SDA/SS -> D2(GPIO 4)
+   // SCK -> D5(GPIO 14)
+   // MOSI -> D7(GPIO 13)
+   // MISO/SCL -> D6(GPIO 12)
+   // IRQ -> NOT CONNECTED
+   // GND -> GND
+   // RST -> D1(GPIO 5)
+   // VCC -> 3.3V
+*/
+
+
+int RFIDfunc() {
+  byte readCard[4];   // Stores scanned ID read from RFID Module
+  if ( mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+
+
+    // Read UID from RFID tag, then convert to unsigned integer array by first converting to String then to integer(keep integer values without converting to ASCII)
+    for ( uint8_t i = 0; i < 4; i++) {
+      readCard[i] = mfrc522.uid.uidByte[i];
+      UID[i] = String(readCard[i]).toInt();
     }
-
+    mfrc522.PCD_StopCrypto1(); // Exit/stop communication with device/tag
   }
-  if (debug == 1) {
-    Serial.println("");
+  else {
+    //setZero(); // Return UID = 0000, when no tag is identified
   }
+  return UID;
+}
 
-
-  if (debug == 1) {
-    Serial.println(F("\n**End Reading**\n"));
+int setZero() {
+  for ( uint8_t i = 0; i < 4; i++) {
+    UID[i] = 0;
   }
-
-
-  mfrc522.PICC_HaltA();
-  mfrc522.PCD_StopCrypto1();
-
-return readCard;
-  // Returnerer UID
+  return UID;
 }
