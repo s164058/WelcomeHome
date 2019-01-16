9843f89f3bddc8019a20d39e283417eff2da4d8a
andreasgamborg <s163939@student.dtu.dk>
2019-01-16 10:59:57 +0100

    BT okay

--- aWelcomeHomeSystems/aWelcomeHomeSystems.ino
+++ aWelcomeHomeSystems/aWelcomeHomeSystems.ino
@@ -1,38 +1,46 @@
-// Std. setup:
+// Std. setup_____________________________________________________________________________________:
+// State definitions
+#define STATE_1 1
+#define STATE_2 2
+#define STATE_3 3
 
+int currentState;
+int nextState;
+unsigned long startTime;
+unsigned long timeElapsed;
+int std_delay = 5; // delay per state in ms
 
+// Bluetooth setup_____________________________________________________________________________________:
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(D6, D7); // (SERIAL_RX, SERIAL_TX) CONNECT TO (BT_TX, BT_RX)
 
-// Bluetooth setup:
-
-
-
-// RFID setup:
+// RFID setup_____________________________________________________________________________________:
 /*
- *
- * Uses MIFARE RFID card using RFID-RC522 reader
- * Uses MFRC522 - Library
- * -----------------------------------------------------------------------------------------
- *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
- *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
- * Signal      Pin          Pin           Pin       Pin        Pin              Pin
- * -----------------------------------------------------------------------------------------
- * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
- * SPI SS      SDA(SS)      10            53        D10        10               10
- * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
- * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
- * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
- *
- *
- *
- * NodeMCU
- * // SDA/SS -> D2(GPIO 4)
- * // SCK -> D5(GPIO 14)
- * // MOSI -> D7(GPIO 13)
- * // MISO/SCL -> D6(GPIO 12)
- * // IRQ -> NOT CONNECTED
- * // GND -> GND
- * // RST -> D1(GPIO 5)
- * // VCC -> 3.3V
+
+   Uses MIFARE RFID card using RFID-RC522 reader
+   Uses MFRC522 - Library
+   -----------------------------------------------------------------------------------------
+               MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+               Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+   Signal      Pin          Pin           Pin       Pin        Pin              Pin
+   -----------------------------------------------------------------------------------------
+   RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+   SPI SS      SDA(SS)      10            53        D10        10               10
+   SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+   SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+   SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+
+
+
+   NodeMCU
+   // SDA/SS -> D2(GPIO 4)
+   // SCK -> D5(GPIO 14)
+   // MOSI -> D7(GPIO 13)
+   // MISO/SCL -> D6(GPIO 12)
+   // IRQ -> NOT CONNECTED
+   // GND -> GND
+   // RST -> D1(GPIO 5)
+   // VCC -> 3.3V
 */
 
 #include <SPI.h>
@@ -46,74 +54,77 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
 byte readCard[4];   // Stores scanned ID read from RFID Module
 byte UID;
 
-// WiFi setup:
+// WiFi setup_____________________________________________________________________________________:
 /*#include <ESP8266WiFi.h>
 
-char ssid[] = SECRET_SSID;   // your network SSID (name)
-char pass[] = SECRET_PASS;   // your network password
-int keyIndex = 0;            // your network key Index number (needed only for WEP)
-WiFiClient  client;
+  char ssid[] = SECRET_SSID;   // your network SSID (name)
+  char pass[] = SECRET_PASS;   // your network password
+  int keyIndex = 0;            // your network key Index number (needed only for WEP)
+  WiFiClient  client;
 
 
-// Thingspeak setup:
-#include "ThingSpeak.h"
-#include "secrets.h"
+  // Thingspeak setup_____________________________________________________________________________________:
+  #include "ThingSpeak.h"
+  #include "secrets.h"
 
-unsigned long myChannelNumber = SECRET_CH_ID;
-const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+  unsigned long myChannelNumber = SECRET_CH_ID;
+  const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
 */
 
-// Sensors setup:
+// Sensors setup_____________________________________________________________________________________:
 
 
 
-// EDB setup:
+// EDB setup_____________________________________________________________________________________:
 
 
 
-// Output setup:
+// Output setup_____________________________________________________________________________________:
 
 
 
-// LCD setup:
+// LCD setup_____________________________________________________________________________________:
 
 
 
 
 void setup() {
-  // Std. setup:
+  // Std. setup_____________________________________________________________________________________:
   Serial.begin(9600); // Starts a serial connection
+  currentState = STATE_1;
+  nextState = STATE_1;
+  startTime = millis();   // Save starting time
 
 
-  // Bluetooth setup:
+  // Bluetooth setup_____________________________________________________________________________________:
+  BT_setup();
 
 
-
-  // RFID setup:
+  // RFID setup_____________________________________________________________________________________:
   SPI.begin(); // Start SPI bus
   mfrc522.PCD_Init(); // Start RFID reader
 
-  // WiFi setup:
+  // WiFi setup_____________________________________________________________________________________:
   //WiFi.mode(WIFI_STA);
 
 
-  // Thingspeak setup:
+  // Thingspeak setup_____________________________________________________________________________________:
   //ThingSpeak.begin(client);  // Initialize ThingSpeak
 
 
-  // Sensors setup:
+  // Sensors setup_____________________________________________________________________________________:
 
 
 
-  // EDB setup:
+  // EDB setup_____________________________________________________________________________________:
 
 
 
-  // Output setup:
+  // Output setup_____________________________________________________________________________________:
 
 
 
-  // LCD setup:
+  // LCD setup_____________________________________________________________________________________:
 
 
 
@@ -121,6 +132,41 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-UID = rfidfunc(mfrc522, 1);
-Serial.println(UID);
+  UID = rfidfunc(mfrc522, 1);
+  Serial.println(UID);
+  //Next state?
+  if (nextState != currentState) {
+    startTime = millis();
+    currentState = nextState;
+  }
+  timeElapsed = millis() - startTime;
+
+  // State Logic
+  switch (currentState) {
+    case STATE_1:
+      Serial.println("Im in state 1");
+
+      if (timeElapsed > 1000) {
+        nextState = STATE_2;
+      }
+      break;
+    case STATE_2:
+      Serial.println("Im in state 2");
+      if (timeElapsed > 1000) {
+        nextState = STATE_3;
+      }
+      break;
+    case STATE_3:
+      Serial.println("Im in state 3");
+      if (timeElapsed > 1000) {
+        nextState = STATE_1;
+      }
+      break;
+    default:
+      Serial.println("Unknown state!");
+      nextState = STATE_1;
+      break;
+  }
+
+  delay(std_delay);
 }
--- aWelcomeHomeSystems/bBluetooth.ino
+++ aWelcomeHomeSystems/bBluetooth.ino
@@ -1,7 +1,3 @@
-// Bluetooth
-#include <SoftwareSerial.h>
-SoftwareSerial BTserial(D6, D7);// (RX, TX)
-
 void BT_setup() {
   Serial.begin(115200);
   BTserial.begin(9600);
@@ -9,30 +5,38 @@ void BT_setup() {
   Serial.println("BTserial started at 9600");
   Serial.println("Running setup . . .");
   delay(50);
-  AT("AT+RENEW");
-  AT("AT+NAMEWelHo");
-  AT("AT+ROLE0");
-  AT("AT+SHOW3");
-  AT("AT+IMME1");
-  AT("AT+SCAN8");
-  AT("AT+RESET");
+  AT("AT", 0);
+  AT("AT+RENEW", 0);
+  AT("AT+NAMEWelcomeHome", 0);
+  //AT("AT+MODE2", 0); <-- seems to break something
+  AT("AT+ROLE0", 0); // slave
+  //AT("AT+SHOW3", 0);
+  //AT("AT+IMME1", 0);
+  AT("AT+NOTI0", 0); // no notifications
+  //AT("AT+NOTP1", 0);
+  AT("AT+RESET", 0);
+  Serial.println("Setup complete");
 }
-void BT_last() {
-  AT("AT+RESET");
+
+void BT_last(char * mac) { // give pointer to save MAC in
+  BTserial.write("AT+RADD?");
+  delay(100);
+  int i = 0;
+  BTserial.find("RADD:");
+  while (BTserial.available()) {
+    mac[i] = BTserial.read();
+    i++;
+  }
+  Serial.println("_________________");
+  Serial.println(mac);
 }
-void AT(char* cmd, int mod) {
+
+void AT(char* cmd, bool output) {
   // mod = 0 output off
   // mod = 1 output on
-  if (mod) {
-    BTserial.write(cmd);
-    Serial.print(cmd);
-    char c = ' ';                 // char for reading input
-    while (BTserial.available()) {
-      c = BTserial.read();
-      Serial.write(c);
-    }
-    Serial.println(". OK!");
-  } else {
+  // mod = 2 only output
+
+  if (output == 0) {
     BTserial.write(cmd);
     Serial.print(cmd);
     while (!BTserial.find("OK")) {
@@ -41,6 +45,37 @@ void AT(char* cmd, int mod) {
     while (BTserial.available() > 0 ) {
       Serial.read();
     }
-    Serial.println(". OK!");
+    Serial.println(" OK");
+  } else if (output == 1) {
+    BTserial.write(cmd);
+    Serial.println(cmd);
+    delay(100);
+    char c = ' ';                 // char for reading input
+    while (BTserial.available()) {
+      c = BTserial.read();
+      Serial.write(c);
+    }
+    Serial.println("");
+  } else if (output == 2) {
+    BTserial.write(cmd);
+    delay(100);
+    char c = ' ';                 // char for reading input
+    while (BTserial.available()) {
+      c = BTserial.read();
+      Serial.write(c);
+    }
+    Serial.println("");
   }
+
 }
+void BT_setting() {
+  Serial.println("Current parameters");
+  AT("AT+NAME?", 2);
+  AT("AT+MODE?", 2);
+  AT("AT+ROLE?", 2);
+  AT("AT+SHOW?", 2);
+  AT("AT+IMME?", 2);
+  AT("AT+NOTI?", 2);
+  AT("AT+NOTP?", 2);
+}
+
