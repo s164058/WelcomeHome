8002dd6f556a98c7c197d0c31eb256e221add32b
andreasgamborg <s163939@student.dtu.dk>
2019-01-17 11:07:09 +0100

    Init LCD

--- aWelcomeHomeSystems/aWelcomeHomeSystems.ino
+++ aWelcomeHomeSystems/aWelcomeHomeSystems.ino
@@ -5,6 +5,8 @@
 #include "Arduino.h"
 #include <EDB.h>    // Extended Database Library
 #include <EEPROM.h> // Use the Internal Arduino EEPROM as storage
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 // Std. setup_____________________________________________________________________________________:
 // State definitions
@@ -73,7 +75,17 @@ int buttonState = 0;
 
 
 // LCD setup_______________________________________________________________________________________
+uint8_t bell[8]  = {0x4, 0xe, 0xe, 0xe, 0x1f, 0x0, 0x4};
+uint8_t note[8]  = {0x2, 0x3, 0x2, 0xe, 0x1e, 0xc, 0x0};
+uint8_t clock[8] = {0x0, 0xe, 0x15, 0x17, 0x11, 0xe, 0x0};
+uint8_t heart[8] = {0x0, 0xa, 0x1f, 0x1f, 0xe, 0x4, 0x0};
+uint8_t duck[8]  = {0x0, 0xc, 0x1d, 0xf, 0xf, 0x6, 0x0};
+uint8_t check[8] = {0x0, 0x1 ,0x3, 0x16, 0x1c, 0x8, 0x0};
+uint8_t cross[8] = {0x0, 0x1b, 0xe, 0x4, 0xe, 0x1b, 0x0};
+uint8_t retarrow[8] = {  0x1, 0x1, 0x5, 0x9, 0x1f, 0x8, 0x4};
 
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 
 
@@ -119,7 +131,7 @@ void setup() {
 
 
   // LCD setup_____________________________________________________________________________________:
-
+ LCD_setup()
 
 
 }
--- aWelcomeHomeSystems/iLCD.ino
+++ aWelcomeHomeSystems/iLCD.ino
@@ -1,1 +1,25 @@
-// LCD
+void LCD_setup()
+{
+  lcd.begin();
+  lcd.backlight();
+  lcd.createChar(0, bell);
+  lcd.createChar(1, note);
+  lcd.createChar(2, clock);
+  lcd.createChar(3, heart);
+  lcd.createChar(4, duck);
+  lcd.createChar(5, check);
+  lcd.createChar(6, cross);
+  lcd.createChar(7, retarrow);
+  lcd.home();
+
+  lcd.print("LCD setup . . .");
+  lcd.setCursor(0, 1);
+  for(int i = 0: i < 8; i++){
+    lcd.write(i);
+    lcd.print(" ");
+  }
+  
+}
+
+
+
