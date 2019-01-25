af01fd33074f450d2b208393684fc48fc7eb0e26
s164058 <46519669+s164058@users.noreply.github.com>
2019-01-25 12:21:12 +0100

    Update cRFID.ino
    Updated pin number for Mega

--- aWelcomeHomeSystems/cRFID.ino
+++ aWelcomeHomeSystems/cRFID.ino
@@ -8,7 +8,7 @@
                Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
    Signal      Pin          Pin           Pin       Pin        Pin              Pin
    -----------------------------------------------------------------------------------------
-   RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+   RST/Reset   RST          9             49        D9         RESET/ICSP-5     RST
    SPI SS      SDA(SS)      10            53        D10        10               10
    SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
    SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
